cd7e2abbe71916ac82ab4edb10f4f5feec5a6262
PN <priteshrna@gmail.com>
2021-02-19 18:24:28 -0500

    config ignored

--- Stalks/ViewController.swift
+++ Stalks/ViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
+    
+    let swifter = Swifter(consumerKey: <#T##String#>, consumerSecret: <#T##String#>, appOnly: <#T##Bool#>)
 
     override func viewDidLoad() {
         super.viewDidLoad()
