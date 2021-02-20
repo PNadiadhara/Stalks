0c4f22d974d07d3bec40a0b274299461176de670
PN <priteshrna@gmail.com>
2021-02-19 19:39:17 -0500

    API call works

--- Stalks/ViewController.swift
+++ Stalks/ViewController.swift
@@ -10,11 +10,19 @@ import SwifteriOS
 
 class ViewController: UIViewController {
     
-    let swifter = Swifter(consumerKey: <#T##String#>, consumerSecret: <#T##String#>, appOnly: <#T##Bool#>)
+    let swifter = Swifter(consumerKey: TwitterAPI.init().APIKey, consumerSecret: TwitterAPI.init().APIKeySeceret)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //only these 3 paramenters needed for now
+        swifter.searchTweet(using: "@Apple") { (results, metadata) in
+            print(results)
+        } failure: { (error) in
+            print("Encountered Error with API Request: \(error)")
+        }
+
+        
     }
 
 
